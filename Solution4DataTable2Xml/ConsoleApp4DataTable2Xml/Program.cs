12427708c12f26fa2c9926df6ffee2b6b20e022a
HUDWARE <udoughong@hotmail.com>
2022-05-16 08:50:52 +0800

    update on 20220516 0850

--- Solution4DataTable2Xml/ConsoleApp4DataTable2Xml/Program.cs
+++ Solution4DataTable2Xml/ConsoleApp4DataTable2Xml/Program.cs
@@ -3,11 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
-using System.Xml.Linq;
 
 namespace ConsoleApp4DataTable2Xml
 {
@@ -16,6 +12,8 @@ namespace ConsoleApp4DataTable2Xml
         static void Main(string[] args)
         {
             #region 產生新資料字串清單(theDataList)
+            //TODO: 確認資料字串清單的格式是否和此範例一致
+            //TODO: 請確認作為相同集合的第二個欄位資料是否一致
             List<string[]> theDataList = new List<string[]>
             {
                 new string[] { "1", "CID1", "VID1-1", "ZID1-2" },
@@ -33,20 +31,45 @@ namespace ConsoleApp4DataTable2Xml
             Class4Event myEvent = new Class4Event();
             #region 產生新資料表(dt)並且匯入資料字串清單(theDataList)
             string theTableName = "DemoDataTable";
+            //TODO: 請確認作為相同集合的欄位資料應該為第二個欄位，第一個欄位是Key，第三個欄位以後才是每一個集合的資料。
             List<string> theColumnArray = new List<string> { "ID", "CID", "VID", "ZID" };
+            //TODO: 請參考此行程式帶入正確的參數到CreateDataTable函式。
             DataTable dt = myEvent.CreateDataTable(theTableName, theColumnArray, theDataList);
             #endregion
             #region 讀取資料表(dt)並轉成XML字串(strXml)
+            //TODO: 對XML的每一筆資料的開頭，請設定一個固定的字串。
             string strOfElementGroupName = "Item";
+            //TODO: 請參考此行程式帶入正確的參數到CreateXmlString函式。
             string strXml = myEvent.CreateXmlString(dt, strOfElementGroupName, theColumnArray);
+            //TODO: 請確認產出的字串是否為如下之未斷行結果。
+            /*
+             * <Root>
+             *  <CID1>
+             *      <Item VID = 'VID1-1' ZID = 'ZID1-2'/>
+             *      <Item VID = 'VID1-2' ZID = 'ZID1-3'/>
+             *      <Item VID = 'VID1-3' ZID = 'ZID1-4'/>
+             *      <Item VID = 'VID1-4' ZID = 'ZID1-5'/>
+             *      <Item VID = 'VID1-5' ZID = 'ZID1-6'/>
+             *  </CID1>
+             *  <CID2>
+             *      <Item VID = 'VID2-1' ZID = 'ZID2-2'/>
+             *      <Item VID = 'VID2-2' ZID = 'ZID2-3'/>
+             *      <Item VID = 'VID2-3' ZID = 'ZID2-4'/>
+             *      <Item VID = 'VID2-4' ZID = 'ZID2-5'/>
+             *      <Item VID = 'VID2-5' ZID = 'ZID2-6'/>
+             *  </CID2>
+             * </Root>
+             */
             #endregion
             #region 讀取XML字串(strXml)並存成XML檔案
+            //TODO: 請將所得到之字串結果轉存成XML檔案。
             string theTargetOfXml = @".\MyNewXml.xml";
             XmlDocument xd = new XmlDocument();
             xd.LoadXml(strXml);
             xd.Save(theTargetOfXml);
             #endregion
             #region 嘗試讀取XML檔案
+            //TODO: 經過驗證後，前面的程式所產出的XML檔案式正確的。
             DBTableSchema.XmlEvent xmlEvent = new DBTableSchema.XmlEvent();
             List<string> theXmlElementArray = new List<string> { "VID", "ZID" };
             List<string[]> ResultOfReadXml = xmlEvent.ReadSingleNodeOfXmlWithElementList(theTargetOfXml, "CID1", "Item", theXmlElementArray);
